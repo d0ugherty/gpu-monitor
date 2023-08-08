aebc1352434cf843075b445c570d9584830473bc
thomas <tdougherty16@gmail.com>
2023-08-07 20:11:28 -0400

    process

--- process.cpp
+++ process.cpp
@@ -1,7 +1,28 @@
 #include <string>
+#include <iostream>
+#include <fstream>
+#include <sys/types.h>
 #include "process.h"
 
 Process::Process(int pid) : pid(pid) {
     readStatFile();
 }
 
+void Process::readStatFile(){
+    std::string stat_filepath = "/proc/" + std::to_string(pid) + "/stat";
+    std::ifstream stat_file(stat_filepath);
+
+    if(!stat_file) {
+        std::cerr << "Failed to find stat file " << pid << std::endl;
+        return;
+    }
+
+    std::string line;
+    std::getline(stat_file, line);
+    stat_file.close();
+
+    std::sscanf(line.c_str(),"%*d %*s %*c %*d %*d %*d %*d %*d %*u %*u %*u %*u %*u %*u %ld %ld", &utime, &stime);
+}
+
+double Process::getCpuUsage(int pid) {
+}    
